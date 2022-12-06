97d20745cdb1b0064c49a471f9fbf54437177b28
Steven <Stevenhuytran03@gmail.com>
2022-12-06 15:44:29 -0800

    Commit to test Vehicle Model Form

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './MainPage';
-import Nav from './Nav';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import VehicleModelForm from "./inventory/VehicleModelForm";
+import MainPage from "./MainPage";
+import Nav from "./Nav";
+import VehicleModelForm from "./inventory/VehicleModelForm";
+
 
 function App() {
   return (
@@ -9,6 +12,7 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="models/new" element={<VehicleModelForm />} />
         </Routes>
       </div>
     </BrowserRouter>
